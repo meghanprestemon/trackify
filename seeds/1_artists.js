4a81bd8bdc25b57ca3adad0c9d34aaf5f3b69639
Meghan Prestemon <m.m.hares@gmail.com>
2017-06-13 13:39:04 -0700

    initial commit

---
+++ seeds/1_artists.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('artists').del()
+    .then(() => {
+      return knex('artists').insert([
+        {id: 1, name: 'The Beatles'},
+        {id: 2, name: 'Adele'}
+      ]);
+    });
+};
