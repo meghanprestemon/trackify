0b9c8231068bc1b807dbfb7afd6a61b3dc78eb74
Meghan Prestemon <m.m.hares@gmail.com>
2017-07-03 22:13:02 -0700

    Route /artists

--- routes/artists.js
+++ routes/artists.js
@@ -2,5 +2,88 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
+
+router.get('/artists', (_req, res, next) => {
+  knex('artists')
+    .orderBy('id')
+    .then((artists) => {
+      res.send(artists);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/artists/:id', (req, res, next) => {
+  knex('artists')
+    .where('id', req.params.id)
+    .first()
+    .then((artist) => {
+      if (!artist) {
+        return next();
+      }
+      res.send(artist);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/artists/:id', (req, res, next) => {
+  knex('artists')
+    .where('id', req.params.id)
+    .first()
+    .then((artist) => {
+      if (!artist) {
+        return next();
+      }
+      return knex('artists')
+        .update({ name: req.body.name }, '*')
+        .where('id', req.params.id);
+    })
+    .then((artists) => {
+      res.send(artists[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/artists/:id', (req, res, next) => {
+  let artist;
+
+  knex('artists')
+    .where('id', req.params.id)
+    .first()
+    .then((row) => {
+      if (!row) {
+        return next();
+      }
+      artist = row;
+      return knex('artists')
+        .del()
+        .where('id', req.params.id);
+    })
+    .then(() => {
+      delete artist.id;
+      res.send(artist);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/artists/:id/tracks', (req, res, next) => {
+  knex('tracks')
+    .where('artist_id', req.params.id)
+    .orderBy('id')
+    .then((track) => {
+      res.send(track);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
